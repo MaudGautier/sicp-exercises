efac5f911c77b68329e52cbed0541715cc4a5b24
Maud Gautier <maud.j.gautier@gmail.com>
2024-02-18 14:57:38 -0500

    feat(chapter1): exercise 11 - iterative

--- chapter1/11.scm
+++ chapter1/11.scm
@@ -16,3 +16,22 @@
 (f-rec 4);11
 (f-rec 5);25
 
+;Iterative process
+(define (f-iter target current a b c)
+  (define new-value (+ a (* 2 b) (* 3 c)))
+  (cond ((< target 3) target)
+        ((= target current) a)
+        (else (f-iter target (+ current 1) new-value a b)))
+  )
+
+
+(define (f-iter-init n)
+  (f-iter n 2 2 1 0)
+  )
+
+(f-iter-init 0);0
+(f-iter-init 1);1
+(f-iter-init 2);2
+(f-iter-init 3);4
+(f-iter-init 4);11
+(f-iter-init 5);25
