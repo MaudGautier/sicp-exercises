8690dc1a218dacdcb2eb13f5ed11d9e3a84c87d6
Maud Gautier <maud.j.gautier@gmail.com>
2024-02-28 10:22:50 -0500

    feat(chapter2): exercise 33 - length

--- chapter2/33.scm
+++ chapter2/33.scm
@@ -4,6 +4,8 @@
        (proc (car sequence)
              (accumulate proc initial (cdr sequence)))))
 
+
+;map
 (define nil '())
 (define (map proc sequence)
   (accumulate (lambda (first accumulator) (cons (proc first) accumulator))
@@ -12,10 +14,18 @@
 
 (map square (list 1 2 3 4));(1 4 9 16)
 
+;append
 (define (append seq1 seq2)
   (accumulate cons seq2 seq1))
 
 (append (list 1 2) (list 3 4));(1 2 3 4)
 
 
+;length
+(define (length sequence)
+  (accumulate (lambda (first x)
+                (+ 1 x))
+    0
+    sequence))
 
+(length (list 1 2 3 4));4
