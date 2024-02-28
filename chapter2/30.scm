f49468136d8942a48a831cc9c77df2a28eb630f8
Maud Gautier <maud.j.gautier@gmail.com>
2024-02-28 09:11:20 -0500

    feat(chapter2): exercise 30 - second method

--- chapter2/30.scm
+++ chapter2/30.scm
@@ -1,17 +1,6 @@
-;For sequences:
-;(define (square-list-1 items)
-;  (if (null? items)
-;      nil
-;      (cons (* (car items) (car items)) (cdr items))))
-;
-;(define (square-list-2 items)
-;  (map (lambda (x) (* x x)) items))
-
-;For trees:
 (define nil '())
 
 (define (square-tree-1 tree)
-  (display tree)
   (cond ((null? tree) nil)
         ((not (pair? tree)) (* tree tree))
         (else (cons (square-tree-1 (car tree))
@@ -19,9 +8,18 @@
     )
   )
 
+(define (square-tree-2 tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+           (square-tree-2 sub-tree)
+           (* sub-tree sub-tree)
+           )
+         )
+    tree)
+  )
 
 (define tree1 (list 1
                    (list 2 (list 3 4) 5)
                    (list 6 7)))
-(square-tree-1 tree1) ;(1 (4 (9 16) 25) (36 49))
-;(square-tree-2 lst1);(1 (4 (9 16) 25) (36 49))
+(square-tree-1 tree1);(1 (4 (9 16) 25) (36 49))
+(square-tree-2 tree1);(1 (4 (9 16) 25) (36 49))
