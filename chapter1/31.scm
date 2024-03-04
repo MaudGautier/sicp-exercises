460f0e0eb90fd5ef128227d7cbe48eadb27d3fcf
Maud Gautier <maud.j.gautier@gmail.com>
2024-03-04 12:38:12 -0500

    feat(chapter1): exercise 31.b

--- chapter1/31.scm
+++ chapter1/31.scm
@@ -17,3 +17,17 @@
 (factorial 3);6
 (factorial 4);24
 (factorial 5);120
+
+(define (product-2 proc start end next)
+  (define (iter i result)
+    (if (> i end)
+      result
+      (iter (next i) (* result (proc i)))
+      )
+    )
+  (iter start 1)
+  )
+
+(product-2 square 1 1 inc);1
+(product-2 square 1 2 inc);4
+(product-2 square 1 3 inc);36
