be7e994376b6ad4ee0b94f6be2175202ff8c951b
Maud Gautier <maud.j.gautier@gmail.com>
2024-03-04 11:18:36 -0500

    feat(chapter1): exercise 25

---
+++ chapter1/25.scm
@@ -0,0 +1,3 @@
+;See explanations at: https://sicp-solutions.net/post/sicp-solution-exercise-1-25/
+;In a nutshell: fast-expt will generate extremely big numbers for the prime numbers we are testing
+;               => this may result in running out of memory.
