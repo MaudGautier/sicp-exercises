36ff6111b765bd472e02942bff95bacd14c662a9
Maud Gautier <maud.j.gautier@gmail.com>
2024-03-04 12:48:51 -0500

    feat(chapter1): exercise 32.b

--- chapter1/32.scm
+++ chapter1/32.scm
@@ -1,3 +1,4 @@
+;Recursive accumulate
 (define (accumulate combiner null-value term a next b)
   (if (> a b)
     null-value
@@ -12,7 +13,7 @@
   (accumulate (lambda (x y) (* x y)) 1 term a next b)
   )
 
-;Testing
+;Testing recursive
 (define (inc n) (+ n 1))
 (sum square 0 inc 1);1
 (sum square 0 inc 2);5
@@ -20,4 +21,31 @@
 (product square 1 inc 1);1
 (product square 1 inc 2);4
 (product square 1 inc 3);36
+
+
+;Iterative accumulate
+(define (accumulate-2 combiner null-value term a next b)
+  (define (iter i result)
+    (if (> i b)
+      result
+      (iter (next i) (combiner result (term i)))
+      ))
+  (iter a null-value)
+  )
+
+(define (sum-2 term a next b)
+  (accumulate-2 (lambda (x y) (+ x y)) 0 term a next b)
+  )
+(define (product-2 term a next b)
+  (accumulate-2 (lambda (x y) (* x y)) 1 term a next b)
+  )
+
+;Testing iterative
+(sum-2 square 0 inc 1);1
+(sum-2 square 0 inc 2);5
+(sum-2 square 0 inc 3);14
+(product-2 square 1 inc 1);1
+(product-2 square 1 inc 2);4
+(product-2 square 1 inc 3);36
+
 
