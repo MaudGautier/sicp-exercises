f6210ea79ddd20027e3e13abba09d16f47cca0f2
Maud Gautier <maud.j.gautier@gmail.com>
2024-03-12 18:42:42 -0400

    feat(chapter2): exercise 29.b

--- chapter2/29.scm
+++ chapter2/29.scm
@@ -4,8 +4,23 @@
 (define (make-branch length structure)
   (list length structure))
 
+;Part A
 (define (left-branch mobile) (car mobile))
 (define (right-branch mobile) (cdr mobile))
 (define (branch-length branch) (car branch))
 (define (branch-structure branch) (cdr branch))
+
+;Part B
+(define (branch-weight branch)
+  (cond
+    ((null? branch) 0)
+    ((not (pair? branch)) branch)
+    (else (total-weight branch))
+    )
+  )
+(define (total-weight mobile)
+  (+ (branch-weight (left-branch mobile)) (branch-weight (right-branch mobile)))
+  )
+(define mob (make-mobile 1 (make-mobile 4 0)))
+(total-weight mob);5
 
