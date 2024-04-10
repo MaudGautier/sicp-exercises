79fd7e7dedd49bf0730b2926f12b72eeec545ae4
Maud Gautier <maud.j.gautier@gmail.com>
2024-04-10 09:38:50 -0400

    feat(chapter2): exercise 65.b

--- chapter2/65.scm
+++ chapter2/65.scm
@@ -13,6 +13,30 @@
   )
 
 
+(define (intersection-tree tree1 tree2)
+  (let (
+         (set1 (tree->list-1 tree1))
+         (set2 (tree->list-1 tree2))
+         )
+    (let ((intersection-result (intersection-set set1 set2)))
+      (list->tree intersection-result))
+    )
+  )
+
+;Helper function
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+    '()
+    (let ((x1 (car set1)) (x2 (car set2)))
+      (cond
+        ((= x1 x2)
+          (cons x1 (intersection-set (cdr set1) (cdr set2))))
+        ((< x1 x2)
+          (intersection-set (cdr set1) set2))
+        ((< x2 x1)
+          (intersection-set set1 (cdr set2)))))))
+
+
 ;Testing
 (define tree1 '(7 (3 (1 () ()) (5 () ())) (9 () (11 () ()))))
 (define tree2 '(5 (3 (1 () ()) ()) (9 (7 () ()) (11 () ()))))
@@ -20,3 +44,5 @@
 (union-tree tree1 tree2) ; Expected: (5 (1 () (3 () ())) (9 (7 () ()) (11 () ())))
 (union-tree tree1 tree3) ; Expected: (6 (3 (0 () (1 () ())) (4 () (5 () ()))) (11 (7 () (9 () ())) (15 () (20 () ()))))
 
+(intersection-tree tree1 tree2) ; Expected: (5 (1 () (3 () ())) (9 (7 () ()) (11 () ())))
+(intersection-tree tree1 tree3) ; Expected: (3 () ())
