fb946cb1582e38f5987c9c9491f324798ffd634e
Maud Gautier <maud.j.gautier@gmail.com>
2024-04-23 19:44:54 -0400

    feat(chapter2): exercise 72

---
+++ chapter2/72.scm
@@ -0,0 +1,3 @@
+;Order of growth in the number of steps to encode a symbol (exercise 2.68) = o(height tree * number of symbols)
+; => o(n^2) in the case of exercise 71 (worst case)
+;Best case: full binary tree => o(log(n) * n)
