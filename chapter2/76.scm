1e44d7af41020bcc97d2e580d2153c3e721d2c85
Maud Gautier <maud.j.gautier@gmail.com>
2024-04-23 22:55:46 -0400

    feat(chapter2): exercise 76

---
+++ chapter2/76.scm
@@ -0,0 +1,8 @@
+;Explicit dispatch:
+;Change every function when a new type is added (add the new type to each)
+
+;Data-directed style:
+;Install a new package when a new type is added (upper-level calls don't change)
+
+;Message-passing style:
+;Add the new type of object that accepts the same operations
