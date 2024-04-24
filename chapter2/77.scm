2d25c4bd717c69c27727716407375784712a40a3
Maud Gautier <maud.j.gautier@gmail.com>
2024-04-24 08:46:00 -0400

    feat(chapter2): exercise 77

---
+++ chapter2/77.scm
@@ -0,0 +1,42 @@
+;From the book - tagged data
+(define (type-tag datum)
+  (if (pair? datum)
+    (car datum)
+    (error "Bad tagged datum: TYPE-TAG" datum)))
+(define (contents datum)
+  (if (pair? datum)
+    (cdr datum)
+    (error "Bad tagged datum: CONTENTS" datum)))
+
+;From the book - apply-generic
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contents args))
+        (error
+          "No method for these types: APPLY-GENERIC"
+          (list op type-tags))))))
+
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+
+;From the book - exercise
+(define z (cons 'complex 'rectangular (cons 3 4)))
+
+;From the book - change by Alyssa
+(put 'real-part '(complex) real-part)
+(put 'imag-part '(complex) imag-part)
+(put 'magnitude '(complex) magnitude)
+(put 'angle '(complex) angle)
+
+;Explanation:
+;(magnitude z)
+; -> (apply-generic 'magnitude z)
+; -> (type-tag z)
+; -> 'complex
+; -> (get 'magnitude 'complex)
+; -> magnitude
+
